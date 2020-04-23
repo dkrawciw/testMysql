f9a6483eb4a08677a870023d437f5ebdc17b32e1
Daniel Krawciw <46691574+dkrawciw@users.noreply.github.com>
2020-04-22 22:46:42 -0600

    ahhhhh

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', function(){
     if(err){
       throw err;
     }
-    console.log('The solution is: ' results[0].solution);
+    console.log('The solution is: ' + results[0].solution);
   });
   res.render('index.html');
 });
