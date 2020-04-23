47bb08edd40201899965a07333aa783346e1781a
Daniel Krawciw <46691574+dkrawciw@users.noreply.github.com>
2020-04-22 22:58:16 -0600

    changed port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var connection = mysql.createConnection({
-  host    : '127.0.0.1',
+  host    : '127.0.0.1:3306',
   user    : 'admin',
   password: '',
   database: 'test'
