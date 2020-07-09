ff1bb67d62f6cf1c1f0acc9f6e1b62e5e7398919
Daniel Krawciw <46691574+dkrawciw@users.noreply.github.com>
2020-07-09 12:53:39 -0600

    like a plastic bag

--- changePassword.sql
+++ changePassword.sql
@@ -1,3 +1,3 @@
 USE mysql;
-update user set authentication_string=password('xD#X3227l!7K&LW4k4av') where user='root';
+update user set authentication_string=password('xD#X3227l!7K&LW4k4av') where user == 'root';
 flush privileges;
